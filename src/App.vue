2d6827d19a73cbf35e1d0d2502be8e8dda1e5225
AlfClark <alfredoclarkcardenas@gmail.com>
2022-06-10 20:41:19 -0700

    Roboto font added

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,26 @@
 <template>
+  <header>
+    <router-link to="/">
+      <h1><span>movie</span>Zone</h1>
+    </router-link>
+  </header>
   <router-view/>
 </template>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300;500;700&display=swap');
+
+:root{
+  --darkBlack: #0C0D18;
+}
+
+*{
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+  font-family: 'Roboto', sans-serif;
+  background-color: var(--darkBlack);
+}
+
 
 </style>
