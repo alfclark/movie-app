4ff77b005ddc7d64e6718326dcc1ee696c3caeb5
AlfClark <alfredoclarkcardenas@gmail.com>
2022-06-19 22:03:28 -0700

    Syntax fix

--- src/App.vue
+++ src/App.vue
@@ -1,78 +1,76 @@
 <template>
   <header>
     <router-link class="logo" to="/">
-      <img class="logoImg" src="./assets/logo.png" alt="">
+      <img src="./assets/logo.png" alt="logo" class="logoImg" />
       <h1 class="logoTitle"><span>movie</span>Zone</h1>
       <p>The place to find where to watch your movies.</p>
     </router-link>
   </header>
   <main>
-    <router-view/>
+    <router-view />
   </main>
 </template>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300;500;700&display=swap');
+@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@300;500;700&display=swap");
 
-:root{
-  --darkBlack: #0C0D18;
-  --lightBlack: #2B2B38;
+:root {
+  --darkBlack: #0c0d18;
+  --lightBlack: #2b2b38;
   --white: #f6f6f6f1;
   --whiteLight: #f6f6f6b0;
   --mutedBlue: #2c3e50;
 }
 
-*{
+* {
   padding: 0;
   margin: 0;
-  box-sizing: border-box; 
+  box-sizing: border-box;
 }
 
 #app {
-  font-family: 'Roboto', sans-serif;
+  font-family: "Roboto", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: ;
   background-color: var(--darkBlack);
   height: 100vh;
 }
 
-a{
+a {
   text-decoration: none;
 }
 
-header{
+header {
   display: flex;
   align-items: center;
   justify-content: center;
   padding: 1rem;
 }
 
-.logo{
+.logo {
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
 }
 
-.logoImg{
+.logoImg {
   width: 50px;
 }
 
-.logoTitle{
+.logoTitle {
   color: var(--white);
   font-size: 1.5rem;
 }
 
-span{
+span {
   font-weight: 300;
 }
 
-p{
+p {
   color: var(--whiteLight);
   font-weight: 300;
-  font-size: .7rem;
+  font-size: 0.7rem;
 }
-
 </style>
