aaba0b06d6fe4d70ce1e01ddbfe2e39b1ddea694
AlfClark <alfredoclarkcardenas@gmail.com>
2022-06-23 20:37:37 -0700

    Footer design

---
+++ src/components/HomeFooter.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="footer">
+    <p>The Movie Zone 2022 ©</p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.footer {
+  width: 100vw;
+  height: 3rem;
+  background-color: var(--lightBlack);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
